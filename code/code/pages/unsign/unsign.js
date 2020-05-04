b126e9cf5a8e95d5b39991c9d81cc40aba8a4539
bobo <45066720+boboxxx@users.noreply.github.com>
2020-05-04 20:57:15 +0800

    注销界面

--- code/code/pages/unsign/unsign.js
+++ code/code/pages/unsign/unsign.js
@@ -5,6 +5,63 @@ Page({
    * 页面的初始数据
    */
   data: {
+    disabled:false,
+    pwd:'',
+    pwdinput:false,
+  },
+  pwdinput: function (e) {
+    this.setData({ pwd: e.detail.value });
+    this.setData({ pwdinput: true });
+    if ( this.data.pwdinput == true) {
+      this.setData({ disabled: false });
+    }
+  },
+  unsign: function (e) {
+    console.log(e);
+    this.setData({ disabled: true});
+    wx.request({
+      url: 'http://localhost/GitHub/RMYP-/code/code/pages/unsign/unsign.php', //示例，非真实接口地址
+      method:"POST",
+      data: {
+        pwd: e.detail.value.pwd,
+      },
+      
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded" // 默认值
+      },
+      
+      success: function (res) {
+        console.log(res);
+        if (res.statusCode == 200) {
+          if (res.data.error == true) {
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'error',
+              duration: 2000
+            })
+          } else {
+           
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'success',
+              duration: 2000
+            })
+            setTimeout(function(){
+              wx.switchTab({
+                url: '../unsign/unsign',
+              })
+            },2000)
+          }
+        }else{
+          wx.showToast({
+            title: '服务器出现错误',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
 
   },
 
