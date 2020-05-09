273371913272933763fc75e11de67d7585fe9159
Goi-27 <60462762+Goi-27@users.noreply.github.com>
2020-05-09 20:31:42 +0800

    update

--- code/code/pages/sign/sign.js
+++ code/code/pages/sign/sign.js
@@ -131,5 +131,70 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  create_sign: function (e) {
+    console.log(e.detail.value)
+    wx.request({
+      url: "http://localhost:8080/..." + e.detail.value["username"] + "&password1=" + e.detail.value["password1"] + "&password2=" + e.detail.value["password2"],
+      data: e.detail.value,
+      success: this.getResult.bind(this)
+    })
+
+  },
+  getResult: function (res) {
+    console.log(res.data);
+    if (res.data == "true") {
+      wx.showToast({
+        title: "注册成功",
+        duration: 2000
+      })
+      wx.switchTab({
+        url: '../login/login',
+      })
+      setTimeout(function () {
+        wx.navigateBack({
+          delta: 2
+        })
+      }, 2000)
+    }
+
+    if (res.data == "-1") {
+      wx.showToast({
+        title: "用户名已存在",
+        icon: 'none',
+        duration: 2000
+      })
+      setTimeout(function () {
+        wx.navigateBack({
+          delta: 2
+        })
+      }, 2000)
+    }
+
+    if (res.data == "1") {
+      wx.showToast({
+        title: "注册信息不为空",
+        icon: 'none',
+        duration: 2000
+      })
+      setTimeout(function () {
+        wx.navigateBack({
+          delta: 2
+        })
+      }, 2000)
+    }
+
+    if ((res.data == "false") || (res.data == "0")) {
+      wx.showToast({
+        title: "前后密码不一致",
+        icon: 'none',
+        duration: 3000
+      })
+      setTimeout(function () {
+        wx.navigateBack({
+          delta: 2
+        })
+      }, 2000)
+    }
+  },
 })
