6d868a469118fa9b1b13d8e020abf11b93026cf8
willianT-liang <57422468+willianT-liang@users.noreply.github.com>
2020-05-15 18:49:55 +0800

    update

--- code/code/pages/search/search.js
+++ code/code/pages/search/search.js
@@ -1,50 +1,30 @@
 
 const app = getApp()
 Page({
-  data: {},
- 
-  //执行点击事件
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    result:'',
+    state:''
+  },
   formSubmit: function (e) {
- 
-    //声明当天执行的
     var that = this;
- 
-    //获取表单所有name=keyword的值
-    var formData = e.detail.value.keyword;
- 
-    //显示搜索中的提示
-    wx.showLoading({
-      title: '搜索中',
-      icon: 'loading'
-    })
- 
-    //向搜索后端服务器发起请求
+    var formData = e.detail.value.id; //获取表单所有name=id的值  
     wx.request({
- 
-      //URL
-      url: 'http://localhost/search.php?keyword=' + formData,
- 
-      //发送的数据
+      url: 'http://localhost/2018-5-24/search.php?id=' + formData,
       data: formData,
- 
-      //请求的数据时JSON格式
-      header: {
-        'Content-Type':'application/json'
-      },
- 
-      //请求成功
+      header: { 'Content-Type': 'application/json' },
       success: function (res) {
- 
-        //控制台打印（开发调试用）
         console.log(res.data)
- 
-        //把所有结果存进一个名为re的数组
         that.setData({
           re: res.data,
         })
- 
-        //搜索成功后，隐藏搜索中的提示
-        wx.hideLoading();
+        wx.showToast({
+          title: '已提交',
+          icon: 'success',
+          duration: 2000
+        })
       }
     })
   },
