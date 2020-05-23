1aa7fab542933a4065c32eebb909624cafbd1bbe
2578039059 <60541577+2578039059@users.noreply.github.com>
2020-05-23 20:58:25 +0800

    Update search.js

--- code/code/pages/search/search.js
+++ code/code/pages/search/search.js
@@ -11,7 +11,6 @@ Page({
   },
   onLoad:function() {
     var that = this
-    //var test = res.data.time
     const db = wx.cloud.database()  
     db.collection('data').where({"_openid":"oKGUL4_f55SAlzzcw1VWUVbyosgU"}).get({
       success:function(res){
@@ -23,29 +22,5 @@ Page({
     })
   },
 
- // res.data[2].time.substring(5, 10)
-      //.substring(0, 10)
-   
- 
-  
-   
-  //   var that = this;
-  //   var formData = e.detail.value.id; //获取表单所有name=id的值  
-  //   wx.request({
-  //     url: 'http://localhost/2018-5-24/search.php?id=' + formData,
-  //     data: formData,
-  //     header: { 'Content-Type': 'application/json' },
-  //     success: function (res) {
-  //       console.log(res.data)
-  //       that.setData({
-  //         re: res.data,
-  //       })
-  //       wx.showToast({
-  //         title: '已提交',
-  //         icon: 'success',
-  //         duration: 2000
-  //       })
-  //     }
-  //   })
   
 })
