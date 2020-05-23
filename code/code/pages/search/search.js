f6918ec8539e543d7b7e26e33886546cbb6b00f8
2578039059 <60541577+2578039059@users.noreply.github.com>
2020-05-23 20:57:45 +0800

    Update search.js

--- code/code/pages/search/search.js
+++ code/code/pages/search/search.js
@@ -1,31 +1,51 @@
-
+wx.cloud.init() 
 const app = getApp()
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    result:'',
-    state:''
+    height:"",
+    time:"",
+    weight:""
   },
-  formSubmit: function (e) {
-    var that = this;
-    var formData = e.detail.value.id; //获取表单所有name=id的值  
-    wx.request({
-      url: 'http://localhost/2018-5-24/search.php?id=' + formData,
-      data: formData,
-      header: { 'Content-Type': 'application/json' },
-      success: function (res) {
-        console.log(res.data)
-        that.setData({
-          re: res.data,
-        })
-        wx.showToast({
-          title: '已提交',
-          icon: 'success',
-          duration: 2000
-        })
+  onLoad:function() {
+    var that = this
+    //var test = res.data.time
+    const db = wx.cloud.database()  
+    db.collection('data').where({"_openid":"oKGUL4_f55SAlzzcw1VWUVbyosgU"}).get({
+      success:function(res){
+       for(let i=0;i<res.data.length;i++){
+        console.log(res.data[i].time.substring(5, 10))
+       }
+        
       }
     })
   },
+
+ // res.data[2].time.substring(5, 10)
+      //.substring(0, 10)
+   
+ 
+  
+   
+  //   var that = this;
+  //   var formData = e.detail.value.id; //获取表单所有name=id的值  
+  //   wx.request({
+  //     url: 'http://localhost/2018-5-24/search.php?id=' + formData,
+  //     data: formData,
+  //     header: { 'Content-Type': 'application/json' },
+  //     success: function (res) {
+  //       console.log(res.data)
+  //       that.setData({
+  //         re: res.data,
+  //       })
+  //       wx.showToast({
+  //         title: '已提交',
+  //         icon: 'success',
+  //         duration: 2000
+  //       })
+  //     }
+  //   })
+  
 })
