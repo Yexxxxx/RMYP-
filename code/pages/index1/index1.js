d584ecc0f30a9723c13158e8a682ec234eb22d6f
Yexxxxx <Yexxxxx@users.noreply.github.com>
2020-05-29 17:13:52 +0800

    啦啦啦啦啦

--- code/pages/index1/index1.js
+++ code/pages/index1/index1.js
@@ -1,79 +1,35 @@
 Page({
 
   data: {
-
-    swiperCurrent: 0,
-
-    indicatorDots: true,
-
-    autoplay: true,
-
-    interval: 3000,
-
-    duration: 800,
-
-    circular:true,
-
-    imgUrls: [
-
-      'https://p3.pstatp.com/large/43700001e49d85d3ab52',
-
-      'https://p3.pstatp.com/large/39f600038907bf3b9c96',
-
-      'https://p3.pstatp.com/large/31fa0003ed7228adf421'
-
-    ],
-
-    links:[
-
-      '../user/user',
-
-      '../user/user',
-
-      '../user/user'
-
-    ]
-
-    
-
+    step:null,
   },
-
-  //轮播图的切换事件
-
-  swiperChange: function (e) {
-
-    this.setData({
-
-      swiperCurrent: e.detail.current
-
+  onLoad:function(){
+    wx.login({
+      complete: (res) => {},
     })
-
   },
-
-  //点击指示点切换
-
-  chuangEvent: function (e) {
-
-    this.setData({
-
-      swiperCurrent: e.currentTarget.id
-
-    })
-
+  onShow:function(){
+    var that = this
+    that.gotweRun()
   },
-
-  //点击图片触发事件
-
-  swipclick: function (e) {
-
-    console.log(this.data.swiperCurrent);
-
-    wx.switchTab({
-
-      url: this.data.links[this.data.swiperCurrent]
-
-    })
-
-  }
-
+  //获取微信步数
+  gotweRun:function(){
+    wx.getWeRunData({
+      success:res=> {
+        //console.log("cloudID:"+res.cloudID)    
+        wx.cloud.callFunction({
+          name: 'weRun',
+          data: {
+            weRunData: wx.cloud.CloudID(res.cloudID)
+          },
+        }).then(resData=>{    
+          that.setData({
+            step:resData.result.event.weRunData.data.stepInfoList[30].step
+          })
+          console.log(resData) //注意这里
+           console.log(resData.result.event.weRunData.data.stepInfoList[30])//今天的步数
+          })
+       },
+  })
+  },
 })
