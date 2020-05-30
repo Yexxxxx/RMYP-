1fc87a32ecbbd00a75caf5a4375e5490c1660c28
Yexxxxx <Yexxxxx@users.noreply.github.com>
2020-05-30 21:11:25 +0800

    相关

--- code/pages/index1/index1.js
+++ code/pages/index1/index1.js
@@ -1,35 +1,12 @@
 Page({
-
   data: {
-    step:null,
+    
   },
   onLoad:function(){
-    wx.login({
-      complete: (res) => {},
-    })
+   
   },
   onShow:function(){
-    var that = this
-    that.gotweRun()
+ 
   },
-  //获取微信步数
-  gotweRun:function(){
-    wx.getWeRunData({
-      success:res=> {
-        //console.log("cloudID:"+res.cloudID)    
-        wx.cloud.callFunction({
-          name: 'weRun',
-          data: {
-            weRunData: wx.cloud.CloudID(res.cloudID)
-          },
-        }).then(resData=>{    
-          that.setData({
-            step:resData.result.event.weRunData.data.stepInfoList[30].step
-          })
-          console.log(resData) //注意这里
-           console.log(resData.result.event.weRunData.data.stepInfoList[30])//今天的步数
-          })
-       },
-  })
-  },
+ 
 })
